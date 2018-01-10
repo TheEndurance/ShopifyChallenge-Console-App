f87f1f4dbc413802cf3cd063c32f4bf8de16e6a0
Rawa Jalal <rawa.jalal@gmail.com>
2018-01-10 02:56:40 -0500

    Documentation, extract cyclical reference algorithm into separate method.

--- ShopifyChallengeConsole/Program.cs
+++ ShopifyChallengeConsole/Program.cs
@@ -1,17 +1,30 @@
-﻿using System;
+﻿/* Shopify Software Engineering Code Challenge
+ * 
+ *      Created By : Rawa Jalal
+ *      Date created: January 10th, 2018 
+ * 
+ */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ShopifyChallengeConsole
 {
+    /// <summary>
+    /// Plain CLR object representing the array of menus
+    /// </summary>
     public class Menus
     {
         public Menu[] menus { get; set; }
     }
 
+    /// <summary>
+    /// Plain CLR object representing the menu properties
+    /// </summary>
     public class Menu
     {
         public int id { get; set; }
@@ -25,6 +38,17 @@ namespace ShopifyChallengeConsole
     {
         static HttpClient client = new HttpClient();
 
+        static string apiUrl =
+            "https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1";
+
+        static List<Menu> invalidMenus = new List<Menu>();
+        static List<Menu> validMenus = new List<Menu>();
+
+        /// <summary>
+        /// Gets the menu JSON object by making a request to API.
+        /// </summary>
+        /// <param name="path">The API url path.</param>
+        /// <returns>A plain CLR Menus object.</returns>
         static async Task<Menus> GetMenuAsync(string path)
         {
             Menus menu = null;
@@ -36,70 +60,13 @@ namespace ShopifyChallengeConsole
             return menu;
         }
 
-        static async Task<string> GetMenuAsStringAsync(string path)
-        {
-            string menu = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                menu = await response.Content.ReadAsStringAsync();
-            }
-            return menu;
-        }
-
-        static async Task RunAsync()
-        {
-            client.BaseAddress = new Uri("https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var menus =
-                await GetMenuAsync("https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1");
-
-            string menuAsStrings =
-                await GetMenuAsStringAsync(
-                    "https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1");
-
-            Console.WriteLine(menuAsStrings);
-
-            List<Menu> invalidMenus = new List<Menu>();
-            List<Menu> validMenus = new List<Menu>();
-
-            for (int i = 0; i < menus.menus.Length; i++)
-            {
-                Menu rootMenu = menus.menus[i];
-                Queue<int> childIds = new Queue<int>();
-                foreach (int id in menus.menus[i].child_ids)
-                {
-                    childIds.Enqueue(id);
-                }
-                
-                while (childIds.Count>0)
-                {
-                    Menu childMenu = GetChildMenu(menus,childIds.Dequeue());
-                    if (childMenu == null) break;
-                    foreach (int childId in childMenu.child_ids)
-                    {
-                        if (childId == rootMenu.id)
-                        {
-                            invalidMenus.Add(rootMenu);
-                        }
-                    }
-                   
-                }
-                validMenus.Add(rootMenu);
-            }
-
-            foreach (var menu in validMenus)
-            {
-                Console.WriteLine(menu.id);
-            }
-            
-        }
-
-
-        static Menu GetChildMenu(Menus menus,int id)
+        /// <summary>
+        /// Lookup function to find children menu based on id.
+        /// </summary>
+        /// <param name="menus">The collection of menus to search through.</param>
+        /// <param name="id">The id of the child menu to find.</param>
+        /// <returns>A Menu CLR object.</returns>
+        static Menu GetChildMenu(Menus menus, int id)
         {
             for (int i = 0; i < menus.menus.Length; i++)
             {
@@ -111,13 +78,82 @@ namespace ShopifyChallengeConsole
             return null;
         }
 
+        /// <summary>
+        /// Main task, finds the invalid and valid menus.
+        /// </summary>
+        /// <returns></returns>
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri(apiUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var menus =
+                await GetMenuAsync(apiUrl);
+            FindCyclicalReferences(menus);
+
+            var unserializedValidMenus = new { valid_menus = new List<Menu>() };
+            foreach (var menu in validMenus)
+            {
+                unserializedValidMenus.valid_menus.Add(menu);
+            }
+            string serializedValidMenus = JsonConvert.SerializeObject(unserializedValidMenus);
+
+
+            var unserializedInvalidMenus = new { invalid_menus = new List<Menu>() };
+            foreach (var menu in invalidMenus)
+            {
+                unserializedInvalidMenus.invalid_menus.Add(menu);
+            }
+            string serializedInvalidMenus = JsonConvert.SerializeObject(unserializedInvalidMenus);
+
+            Console.WriteLine(serializedValidMenus);
+            Console.WriteLine(serializedInvalidMenus);
+
+        }
+
+        /// <summary>
+        /// Determines which menus have cyclical references.
+        /// </summary>
+        /// <param name="menus">The Menu collection to search through.</param>
+        private static void FindCyclicalReferences(Menus menus)
+        {
+            for (int i = 0; i < menus.menus.Length; i++)
+            {
+                Menu rootMenu = menus.menus[i];
+                Queue<int> childIds = new Queue<int>();
+                foreach (int id in menus.menus[i].child_ids)
+                {
+                    childIds.Enqueue(id);
+                }
+
+                while (childIds.Count > 0)
+                {
+                    Menu childMenu = GetChildMenu(menus, childIds.Dequeue());
+                    if (childMenu == null) break;
+                    foreach (int childId in childMenu.child_ids)
+                    {
+                        if (childId == rootMenu.id)
+                        {
+                            invalidMenus.Add(rootMenu);
+                        }
+                    }
+
+                }
+                validMenus.Add(rootMenu);
+            }
+        }
+
+        /// <summary>
+        /// Main program entry.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-
             RunAsync().GetAwaiter().GetResult();
             Console.ReadLine();
         }
-
        
     }
 }
